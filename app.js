974abc4a55b940ae60542cd7986b467b7ce24380
Haley <h_ostrander@outlook.com>
2018-01-29 12:20:07 -0500

    updating structure

--- app.js
+++ app.js
@@ -2,20 +2,22 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 // this is a route. this points at the homepage / root
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
-});
+app.use(require('./routes/index'));
+app.use(require('./routes/contact'));
+app.use(require('./routes/users'));
 
 // set up a contact route
-app.get('/contact', (req, res) => {
-	res.sendFile(__dirname + '/contact.html');
-});
+// app.get('/contact', (req, res) => {
+// 	res.sendFile(__dirname + '/contact.html');
+// });
 
 // setup a portfolio route
-app.get('/portfolio', (req, res) => {
-	res.sendFile(__dirname + '/portoflio.html');
-});
+// app.get('/portfolio', (req, res) => {
+// 	res.sendFile(__dirname + '/portoflio.html');
+// });
 
 app.listen(3000, () => {
 	console.log('app running on port 3000');
